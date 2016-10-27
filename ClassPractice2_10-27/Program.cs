371b08970fda542b701834a72acc5ce9c63ff6b9
Tommy Lewis <lewisc0021@gmail.com>
2016-10-27 11:30:52 -0400

    initial commit

---
+++ ClassPractice2_10-27/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPractice2_10_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fact(5));
+        }
+        static int Fact(int x)
+        {
+            if (x == 1)
+                return 1;
+            return x*Fact(x-1);           
+        }
+    }
+}
